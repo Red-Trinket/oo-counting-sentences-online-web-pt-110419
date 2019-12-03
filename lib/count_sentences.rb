1eee07ff38428bf157af154b98054495d57eebda
Matthew Jones <matts3371@gmail.com>
2019-12-03 17:30:14 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -6,16 +6,24 @@ class String
     if self.end_with?(".")
       true 
     else
-      FALSE
+      false 
     end
   end
 
   def question?
-
+    if self.end_with("?")
+      true
+    else 
+      false 
+    end
   end
 
   def exclamation?
-
+    if self.end_with("!")
+      true
+    else
+      false
+    end
   end
 
   def count_sentences
